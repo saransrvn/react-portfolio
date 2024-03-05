92bf7d3170e45f3d9b3d527bf4476566c2b5af6d
saransrvn <srvn1251@gmail.com>
2024-03-05 20:12:39 +0530

    made some changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-icons': '@react-icons/all-files'
+    }
+  },
   build:{
     rollupOptions: {
       external: ['react-icons']
